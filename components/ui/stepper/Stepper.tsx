05c0bb845ffee107e6aa5f2769667898b6286aa2
veronika.markovic.vicert <veronika.markovic@vicert.com>
2023-03-10 12:24:12 +0100

    fixed pr comments suggestions

--- components/ui/stepper/Stepper.tsx
+++ components/ui/stepper/Stepper.tsx
@@ -16,11 +16,6 @@ const steps = ['Enter personal data', 'Select sympthoms'];
 
 export const Stepper: React.FC<Props> = ({activeStepProps, setActiveStepProps}) => {
   const [activeStep, setActiveStep] = React.useState(0);
-  // const [skipped, setSkipped] = React.useState(new Set<number>());
-
-  // const isStepOptional = (step: number) => {
-  //   return step === 1;
-  // };
 
   const handleReset = () => {
     setActiveStep(0);
@@ -35,42 +30,10 @@ export const Stepper: React.FC<Props> = ({activeStepProps, setActiveStepProps}) 
     // eslint-disable-next-line
   }, [activeStepProps]);
 
-  // const isStepSkipped = (step: number) => {
-  //   return skipped.has(step);
-  // };
-
-  // const handleNext = () => {
-  //   // let newSkipped = skipped;
-  //   // if (isStepSkipped(activeStep)) {
-  //   //   newSkipped = new Set(newSkipped.values());
-  //   //   newSkipped.delete(activeStep);
-  //   // }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setActiveStepProps((prevActiveStep: number) => prevActiveStep + 1);
-  //   // setSkipped(newSkipped);
-  // };
-
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
     setActiveStepProps((prevActiveStep: number) => prevActiveStep - 1);
   };
-
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     // You probably want to guard against something like this,
-  //     // it should never occur unless someone's actively trying to break something.
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setActiveStepProps((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
 
   return (
     <Box sx={{width: '80%'}}>
@@ -80,12 +43,6 @@ export const Stepper: React.FC<Props> = ({activeStepProps, setActiveStepProps}) 
           const labelProps: {
             optional?: React.ReactNode;
           } = {};
-          // if (isStepOptional(index)) {
-          //   labelProps.optional = <Typography variant="caption">Optional</Typography>;
-          // }
-          // if (isStepSkipped(index)) {
-          //   stepProps.completed = false;
-          // }
           return (
             <Step key={label} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
@@ -94,15 +51,6 @@ export const Stepper: React.FC<Props> = ({activeStepProps, setActiveStepProps}) 
         })}
       </MaterialStepper>
       {activeStep < steps.length && (
-        //   <React.Fragment>{handleReset()}</React.Fragment>
-        // ) : (
-        // <React.Fragment>
-        //   <Typography sx={{mt: 2, mb: 1}}>All steps completed - you&apos;re finished</Typography>
-        //   <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
-        //     <Box sx={{flex: '1 1 auto'}} />
-        //     <Button onClick={handleReset} text="Reset" variant="text" />
-        //   </Box>
-        // </React.Fragment>
         <>
           <Typography sx={{mt: 2, mb: 1}}>Step {activeStep + 1}</Typography>
           <Box sx={styles.backButtonContainer}>
@@ -116,20 +64,6 @@ export const Stepper: React.FC<Props> = ({activeStepProps, setActiveStepProps}) 
             />
 
             <Box sx={{flex: '1 1 auto'}} />
-            {/* {isStepOptional(activeStep) && (
-              <Button
-                variant="text"
-                color="inherit"
-                onClick={handleSkip}
-                sx={{mr: 1}}
-                text="Skip"
-              />
-            )} */}
-            {/* <Button
-              variant="text"
-              onClick={handleNext}
-              text={activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            /> */}
           </Box>
         </>
       )}
