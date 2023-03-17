c159d1cfa07e1fa869a661f4545b56d74f65810f
veronika.markovic.vicert <veronika.markovic@vicert.com>
2023-03-17 11:22:58 +0100

    added function for creating new date and converting it to string

--- utility/dateUtilities.ts
+++ utility/dateUtilities.ts
@@ -5,3 +5,8 @@ export const convertToISOString = (date: Date) => {
 export const getOnlyDate = (date: string) => {
   return date.slice(0, 10);
 };
+
+export const createStringDate = (date?: string) => {
+  if (date) return new Date(date).toString();
+  return new Date().toString();
+};
