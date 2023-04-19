98bde16d1ac5053cd700b6cb901dc5d342a40bad
veronikamarkovic <veronika.markovic@vicert.com>
2023-04-19 10:51:11 +0200

    added test that checks if the symptoms form is being rendered on the step change

--- components/__tests__/AppointmentForm.test.tsx
+++ components/__tests__/AppointmentForm.test.tsx
@@ -1,7 +1,33 @@
-import {render, screen} from '@testing-library/react';
-
+import {act, fireEvent, render, screen, waitFor, within} from '@testing-library/react';
+import useHttpMock from 'hooks/useHttp';
 import {AppointmentForm} from 'components/appointmentForm/AppointmentForm';
 import {withLocalizationProvider} from 'jest.setup';
+
+jest.mock('hooks/useHttp', () =>
+  jest.fn(() => ({
+    data: null,
+    error: null,
+    isLoading: false,
+    sendRequest: jest.fn(),
+  })),
+);
+
+const examFieldsData = [
+  {id: 1, value: 'Gynaecology'},
+  {id: 2, value: 'Pulmology'},
+];
+const examTypesData = [
+  {id: 1, value: 'CT Scan'},
+  {id: 2, value: 'Ultrasound'},
+];
+const availableTimesData = [
+  {id: 1, value: '9:00 AM'},
+  {id: 2, value: '10:00 AM'},
+  {id: 3, value: '11:00 AM'},
+  {id: 4, value: '12:00 AM'},
+  {id: 5, value: '13:00 AM'},
+  {id: 6, value: '14:00 AM'},
+];
 
 describe('AppointmentForm', () => {
   it('renders the form title', () => {
@@ -29,66 +55,93 @@ describe('AppointmentForm', () => {
     expect(examFieldInput).toBeInTheDocument();
   });
 
-  // it('renders Symptoms Form when next button is clicked and step is changed to 1', async () => {
-  //   // act(() => {
-  //   render(withLocalizationProvider(<AppointmentForm />));
-  //   // });
-  //   expect(screen.getByTestId('nextButton')).toBeInTheDocument();
+  it('renders Symptoms Form when next button is clicked and step is changed to 1', async () => {
+    (useHttpMock as jest.Mock)
+      .mockReturnValueOnce({
+        data: examFieldsData,
+        error: null,
+        isLoading: false,
+        sendRequest: jest.fn(),
+      })
+      .mockReturnValueOnce({
+        data: examTypesData,
+        error: null,
+        isLoading: false,
+        sendRequest: jest.fn(),
+      })
+      .mockReturnValueOnce({
+        data: availableTimesData,
+        error: null,
+        isLoading: false,
+        sendRequest: jest.fn(),
+      });
+    render(withLocalizationProvider(<AppointmentForm />));
 
-  //   const firstNameInput = screen.getByTestId('firstName');
-  //   fireEvent.change(firstNameInput, {target: {value: 'John'}});
+    const firstNameInput = screen.getByTestId('firstName');
+    fireEvent.change(firstNameInput, {target: {value: 'John'}});
 
-  //   const lastNameInput = screen.getByTestId('lastName');
-  //   fireEvent.change(lastNameInput, {target: {value: 'Doe'}});
+    const lastNameInput = screen.getByTestId('lastName');
+    fireEvent.change(lastNameInput, {target: {value: 'Doe'}});
 
-  //   const phoneNumberInput = screen.getByTestId('phoneNumber');
-  //   fireEvent.change(phoneNumberInput, {target: {value: '123-456-7890'}});
+    const phoneNumberInput = screen.getByTestId('phoneNumber');
+    fireEvent.change(phoneNumberInput, {target: {value: '123-456-7890'}});
 
-  //   const emailInput = screen.getByTestId('email');
-  //   fireEvent.change(emailInput, {target: {value: 'johndoe@example.com'}});
+    const emailInput = screen.getByTestId('email');
+    fireEvent.change(emailInput, {target: {value: 'johndoe@example.com'}});
 
-  //   const dateOfBirthInput = screen.getByTestId('dateOfBirth');
-  //   fireEvent.change(dateOfBirthInput, {target: {value: '1990-01-01'}});
+    const dateOfBirthInput = screen.getByTestId('dateOfBirth');
+    fireEvent.change(dateOfBirthInput, {target: {value: '10/12/1998'}});
 
-  //   const firstTimeVisitInput = screen.getByTestId('firstTimeVisit-Yes');
-  //   fireEvent.click(firstTimeVisitInput);
+    const firstTimeVisitInput = screen.getByTestId('firstTimeVisit-Yes');
+    fireEvent.click(firstTimeVisitInput);
 
-  //   const appointmentDateInput = screen.getByTestId('dateOfAppointment');
-  //   fireEvent.change(appointmentDateInput, {target: {value: '2023-04-10'}});
+    const appointmentDateInput = screen.getByTestId('dateOfAppointment');
+    fireEvent.change(appointmentDateInput, {target: {value: '10/04/2023'}});
 
-  //   const user = userEvent.setup();
-  //   const selectButton = screen.getByTestId('examField');
-  //   user.click(selectButton);
-  //   const listbox = document.body.querySelector('ul[role=listbox]');
-  //   const listItem = within(listbox).getByText(optionText);
-  //   user.click(listItem);
-  //   // const selectButton = screen.getByTestId('examField');
-  //   // const examFieldInput = screen.getByTestId('examField');
-  //   // const selectButton = within(examFieldInput).getByRole('button');
-  //   // fireEvent.mouseDown(selectButton);
-  //   // const options = screen.getAllByRole('option');
-  //   // // screen.debug(getAllByRole("option"));
-  //   // fireEvent.mouseDown(options[1]);
-  //   // options[1].click();
-  //   // examFieldInput.value = 'Radiology';
-  //   // fireEvent.change(examFieldInput);
-  //   // expect(examFieldInput).toHaveValue('Radiology');
-  //   // const examFieldInput = screen.getByTestId('examField');
-  //   // fireEvent.change(examFieldInput, {target: {value: 'Radiology'}});
+    const examFieldSelect = screen.getByTestId('form-control-examField');
+    const examFieldSelectButton = within(examFieldSelect).getByRole('button', {hidden: true});
+    fireEvent.mouseDown(examFieldSelectButton);
+    expect(screen.getByRole('listbox', {hidden: false})).not.toEqual(null);
 
-  //   // const examTypeInput = screen.getByTestId('examType');
-  //   // // fireEvent.change(examTypeInput, {target: {value: 'CT Scan'}});
-  //   // examTypeInput.value = 'CT Scan';
-  //   // fireEvent.change(examTypeInput);
-  //   // expect(examTypeInput).toHaveValue('CT Scan');
+    act(() => {
+      const options = screen.getAllByRole('option');
+      fireEvent.mouseDown(options[1]);
+      options[1].click();
+    });
 
-  //   fireEvent.click(screen.getByTestId('nextButton'));
-  //   expect(screen.getByTestId('nextButton')).toBeInTheDocument();
-  //   screen.debug(null, 20000);
-  //   await waitFor(() => {
-  //     expect(screen.getByTestId('symptoms-form-title')).toBeInTheDocument();
-  //   });
-  //   // expect(screen.getByTestId('firstName')).toBeInTheDocument();
-  // });
-  //to-do: add test for the symptoms form rendering when the step changes to 1
+    const examFieldInput = screen.getByTestId('examField') as HTMLInputElement;
+    await waitFor(() => {
+      expect(examFieldInput.value).toEqual('Pulmology');
+    });
+    const examTypeSelect = screen.getByTestId('form-control-examType');
+    const examTypeSelectButton = within(examTypeSelect).getByRole('button');
+    act(() => {
+      examTypeSelectButton.focus();
+      fireEvent.mouseDown(examTypeSelectButton);
+    });
+
+    expect(screen.getByRole('listbox', {hidden: false})).not.toEqual(null);
+
+    act(() => {
+      const options = screen.getAllByRole('option');
+      fireEvent.mouseDown(options[1]);
+      options[1].click();
+    });
+    const examTypeInput = screen.getByTestId('examType') as HTMLInputElement;
+    expect(examTypeInput.value).toEqual('Ultrasound');
+
+    const time = screen.getByTestId('pickedTime-10:00 AM');
+    act(() => {
+      fireEvent.click(time);
+    });
+
+    const nextButton = screen.getByTestId('nextButton');
+    expect(nextButton).toBeInTheDocument();
+
+    fireEvent.click(nextButton);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('symptoms-form')).toBeInTheDocument();
+    });
+  });
 });
